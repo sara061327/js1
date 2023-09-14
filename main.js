23544f63bc02ec45c9c14b98af7b6bb5444d8959
EC2 Default User <ec2-user@ip-172-31-15-121.ap-southeast-2.compute.internal>
2023-09-14 16:06:38 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,60 @@
+var startButton;    
+var stopButton;     
+var resetButton;    
+var showTime;       
+
+var timer;              
+var startTime;          
+var elapsedTime = 0;    
+var holdTime = 0;       
+
+
+window.onload = function () {
+    startButton = document.getElementById("start");
+    stopButton = document.getElementById("stop");
+    resetButton = document.getElementById("reset");
+    showTime = document.getElementById("time");
+}
+
+function start(){
+  var startTime = Date.now();
+  
+　　　measureTime();
+　　　
+　　startButton.disabled = true;
+    stopButton.disabled = false;
+    resetButton.disabled = false;
+}
+
+function stop(){
+    clearInterval(timer);
+
+holdTime += Date.now() - startTime;
+
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    resetButton.disabled = false;
+}
+
+function reset(){
+    clearInterval(timer);
+    
+    elapsedTime = 0;
+    holdTime = 0;
+    showTime.textContent = "00:00.000";
+
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    resetButton.disabled = true;
+}
+
+function measureTime() {
+
+    timer = setTimeout(function () {
+       
+        elapsedTime = Date.now() - startTime + holdTime;
+        showTime.textContent = new Date(elapsedTime).toISOString().slice(14, 23);
+     
+        measureTime();
+    }, 10);
+}
